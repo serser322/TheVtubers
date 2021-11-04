3151e501ecef1de9e1b76c94bd2bcb6e7201783c
serser322 <joess.cts02@g2.nctu.edu.tw>
2021-11-05 03:02:08 +0800

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:serser322/TheVtubers.git master:gh-pages
+git push -f git@github.com:serser322/TheVtubers.git main:gh-pages
 
 cd -
